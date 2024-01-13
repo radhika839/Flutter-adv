75dad53a34f22a7ed2261592fd8d453ba781d4bf
1010radhika <radhikajadhav1010@gmail.com>
2024-01-13 18:27:56 +0530

    poject added

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -8,77 +8,106 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  String? validateEmail(value) {
+    if (value!.isEmpty) {
+      return 'Please Enter An Email';
+    }
+    RegExp emailRegExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!emailRegExp.hasMatch(value)) {
+      return 'please Enter A Valid Email';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
-        child: Column(
-          children: [
-            Text('Welcome Back'),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-              child: TextFormField(
-                decoration: InputDecoration(
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              Text('Welcome Back'),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
                     label: Text('Email'),
-                    border:
-                        OutlineInputBorder(borderRadius: BorderRadius.zero)),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    label: Text('Password'),
-                    border:
-                        OutlineInputBorder(borderRadius: BorderRadius.zero)),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              height: 40,
-              width: 330,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/welcome');
-                },
-                child: Text(
-                  'Sign In',
-                  style: TextStyle(color: Colors.white, fontSize: 17),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.zero),
+                  ),
+                  validator: validateEmail,
                 ),
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))),
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Not a member?'),
-                TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/signup');
-                    },
-                    child: Text(
-                      'Register now',
-                      style: TextStyle(color: Colors.blue.shade500),
-                    ))
-              ],
-            )
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: TextFormField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    label: Text('Password'),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.zero),
+                  ),
+                  validator: (value) {
+                    if (value!.length < 3) {
+                      return 'PassWord is too short';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 40,
+                width: 330,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formkey.currentState!.validate()) {
+                      Navigator.pushNamed(context, '/welcome');
+                    }
+                  },
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white, fontSize: 17),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a member?'),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      child: Text(
+                        'Register now',
+                        style: TextStyle(color: Colors.blue.shade500),
+                      ))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -8,94 +8,135 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  String? validateEmail(value) {
+    if (value!.isEmpty) {
+      return 'Please Enter An Email';
+    }
+    RegExp emailRegExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!emailRegExp.hasMatch(value)) {
+      return 'please Enter A Valid Email';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
-        child: Column(
-          //   mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Let's Create An Account For You",
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-              child: TextFormField(
-                decoration: InputDecoration(
+        child: Form(
+          key: _formkey,
+          child: Column(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Let's Create An Account For You",
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
                     label: Text('Email'),
-                    border:
-                        OutlineInputBorder(borderRadius: BorderRadius.zero)),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    label: Text('Password'),
-                    border:
-                        OutlineInputBorder(borderRadius: BorderRadius.zero)),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    label: Text('Confirm Password'),
-                    border:
-                        OutlineInputBorder(borderRadius: BorderRadius.zero)),
-              ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            SizedBox(
-              height: 40,
-              width: 330,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/signin');
-                },
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(fontSize: 17, color: Colors.white),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.zero),
+                  ),
+                  validator: validateEmail,
                 ),
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))),
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Already Have An Account?'),
-                TextButton(
-                    onPressed: () {
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: TextFormField(
+                  obscureText: true,
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                      label: Text('Password'),
+                      border:
+                          OutlineInputBorder(borderRadius: BorderRadius.zero)),
+                  validator: (value) {
+                    if (value!.length < 3) {
+                      return 'Password Is Too Short';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: TextFormField(
+                  obscureText: true,
+                  controller: confirmPasswordController,
+                  decoration: InputDecoration(
+                    label: Text('Confirm Password'),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.zero),
+                  ),
+                  validator: (value) {
+                    if (value!.length < 3) {
+                      return 'Password Is Too Short';
+                    } else if (value != passwordController.text) {
+                      return 'Enter Correct Password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                height: 40,
+                width: 330,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formkey.currentState!.validate()) {
                       Navigator.pushNamed(context, '/signin');
-                    },
-                    child: Text(
-                      'Login Now',
-                      style: TextStyle(color: Colors.blue.shade500),
-                    ))
-              ],
-            )
-          ],
+                    }
+                  },
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(fontSize: 17, color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Already Have An Account?'),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signin');
+                      },
+                      child: Text(
+                        'Login Now',
+                        style: TextStyle(color: Colors.blue.shade500),
+                      ))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
